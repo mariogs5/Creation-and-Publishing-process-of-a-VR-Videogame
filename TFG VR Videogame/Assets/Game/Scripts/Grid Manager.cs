420f8bf2ec2ceaaea7802a7c397ae7a8afc9d99d
Mario Garcia Sutil <mariosutil2003@gmail.com>
2025-01-01 23:08:09 +0100

    Start of Object Pooling implementation

--- TFG VR Videogame/Assets/Game/Scripts/Grid Manager.cs
+++ TFG VR Videogame/Assets/Game/Scripts/Grid Manager.cs
@@ -4,49 +4,48 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField, Range(1, 3)] private int rows;
-    [SerializeField, Range(1, 5)] private int columns;
+    [SerializeField, Range(1, 3)] private int maxRows;
+    [SerializeField, Range(1, 5)] private int maxColumns;
+
+    private int rows;
+    private int columns;
 
     [SerializeField] private GameObject tilePrefab; // Tile Prefab
     [SerializeField] private float spacing = 1.5f;  // Spacing between tiles
 
+    private List<GameObject> slotPool;
+
     private void Start()
     {
-        GenerateGrid();
+        //GenerateGrid();
     }
 
+    private void UpdateGrid(int newRows, int newColumns)
+    {
+        //rows = newRows;
+        //columns = newColumns;
+        //GenerateGrid();
+    }
+
+    #region Inspector Buttons (Generate & Delete Tiles GO)
     public void GenerateGrid()
     {
         DeleteGrid();
 
         // Calculate the grid Size
-        float startX = -(columns - 1) * spacing / 2f;
-        float startZ = (rows - 1) * spacing / 2f;
+        float startX = -(maxColumns - 1) * spacing / 2f;
+        float startZ = (maxRows - 1) * spacing / 2f;
 
         // Instantiate Tiles
-        for (int row = 0; row < rows; row++)
+        for (int row = 0; row < maxRows; row++)
         {
-            for (int col = 0; col < columns; col++)
+            for (int column = 0; column < maxColumns; column++)
             {
-                Vector3 position = new Vector3(startX + col * spacing, 0, startZ - row * spacing) + transform.position;
+                Vector3 position = new Vector3(startX + column * spacing, 0, startZ - row * spacing) + transform.position;
                 GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity, transform);
-                tile.name = $"Slot ({row},{col})";
-
-                // Color Test
-                //Color randomColor = new Color(Random.value, Random.value, Random.value);
-                //Material randomMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
-                //randomMaterial.color = randomColor;
-                //tile.GetComponent<Renderer>().material = randomMaterial;
+                tile.name = $"Slot ({row},{column})";
             }
         }
-    }
-
-    // Método para actualizar la grid dinámicamente
-    private void UpdateGrid(int newRows, int newColumns)
-    {
-        rows = newRows;
-        columns = newColumns;
-        GenerateGrid();
     }
 
     public void DeleteGrid()
@@ -56,4 +55,5 @@ public class GridManager : MonoBehaviour
             DestroyImmediate(transform.GetChild(i).gameObject);
         }
     }
+    #endregion
 }
