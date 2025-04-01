66e3bcd05bcdc99369cb22ab654d4d14079ee5ea
Mario Garcia Sutil <mariosutil2003@gmail.com>
2025-04-01 18:57:57 +0200

    Vegetables spawn correctly & First iteration of Vegetable Basket

--- TFG VR Videogame/Assets/Game/Scripts/Vegetable.cs
+++ TFG VR Videogame/Assets/Game/Scripts/Vegetable.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Vegetable : MonoBehaviour
 {
+    public static Action OnDunk { get; set; }
+
     private Color startColor = Color.white;
     private Color endColor = Color.green;
     private float duration = 2f;
@@ -63,5 +66,14 @@ public class Vegetable : MonoBehaviour
         objectRenderer.material.color = startColor;
         isActive = true;
         timer = 0f;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "VeggieBasket")
+        {
+            gameObject.SetActive(false);
+            OnDunk?.Invoke();
+        }
     }
 }
