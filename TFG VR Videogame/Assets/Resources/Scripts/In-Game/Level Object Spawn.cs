bb401bffc0f1f9d4e2a9a619b7c1d7060fcc4c81
Mario Garcia Sutil <mariosutil2003@gmail.com>
2025-08-01 19:32:48 +0200

    Level Spawn Script WIP

---
+++ TFG VR Videogame/Assets/Resources/Scripts/In-Game/Level Object Spawn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelObjectSpawn : MonoBehaviour
+{
+    private Vector3 spawn_1 , spawn_2, spawn_3, spawn_4, spawn_5, spawn_6, spawn_7, spawn_8, spawn_9, spawn_10;
+    private void Awake()
+    {
+        spawn_1 = new Vector3(0, 0, 0);
+        spawn_2 = new Vector3(0, 0, 0);
+        spawn_3 = new Vector3(0, 0, 0);
+        spawn_4 = new Vector3(0, 0, 0);
+        spawn_5 = new Vector3(0, 0, 0);
+        spawn_6 = new Vector3(0, 0, 0);
+        spawn_7 = new Vector3(0, 0, 0);
+        spawn_8 = new Vector3(0, 0, 0);
+        spawn_9 = new Vector3(0, 0, 0);
+        spawn_10 = new Vector3(0, 0, 0);
+    }
+}
